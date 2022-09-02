38dc3fefaac213c13c4ad7c7375c014121b446e9
rgomide <renato.s.gomide@gmail.com>
2022-09-01 21:27:29 -0300

    .

--- 02-list-building-with-style/App.js
+++ 02-list-building-with-style/App.js
@@ -1,4 +1,4 @@
-import { View, Text, StatusBar } from 'react-native'
+import { View, Text, StatusBar, StyleSheet } from 'react-native'
 import ListScreen from './src/screens/ListScreen';
 import ListScreenKeyManually from './src/screens/ListScreenKeyManually';
 import ListScreenKeyExtractor from './src/screens/ListScreenKeyExtractor';
@@ -7,15 +7,23 @@ import ListScreenStyled from './src/screens/ListScreenStyled';
 export default function App() {
   return (
     <View>
-      <Text>A simple FlatList</Text>
-      <ListScreen/>
-      <Text>List screen key manually</Text>
-      <ListScreenKeyManually/>
-      <Text>List screen with keyExtractor</Text>
-      <ListScreenKeyExtractor/>
-      <Text>Styled list screen</Text>
-      <ListScreenStyled/>
+      <Text style={styles.textStyle}>A simple FlatList</Text>
+      <ListScreen />
+      <Text style={styles.textStyle}>List screen key manually</Text>
+      <ListScreenKeyManually />
+      <Text style={styles.textStyle}>List screen with keyExtractor</Text>
+      <ListScreenKeyExtractor />
+      <Text style={styles.textStyle}>Styled list screen</Text>
+      <ListScreenStyled />
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontWeight: 'bold',
+    marginTop: 30,
+    marginBottom: 5
+  }
+})
