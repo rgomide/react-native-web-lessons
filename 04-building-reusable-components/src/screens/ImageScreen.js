4f4768cbd6bf8c0ab7a3f6134530838209a5a478
rgomide <renato.s.gomide@gmail.com>
2022-09-29 15:30:54 -0300

    .

--- 04-building-reusable-components/src/screens/ImageScreen.js
+++ 04-building-reusable-components/src/screens/ImageScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import ImageDetail from '../components/ImageDetail'
 
 const ImageScreen = () => {
