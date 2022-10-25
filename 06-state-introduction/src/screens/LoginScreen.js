5833422210ac3bee4d222026aa7bd5900c6a293f
rgomide <renato.s.gomide@gmail.com>
2022-10-25 14:30:49 -0300

    .

--- 06-state-introduction/src/screens/LoginScreen.js
+++ 06-state-introduction/src/screens/LoginScreen.js
@@ -1,10 +1,19 @@
 import React from "react"
-import {Text} from 'react-native'
+import { Text, View, TextInput, Button, StyleSheet } from 'react-native'
 
 const LoginScreen = () => {
     return (
-        <Text>Hello!</Text>
+        <View>
+            <Text>User</Text>
+            <TextInput />
+            <Text>Password</Text>
+            <TextInput secureTextEntry={true} />
+            <Button title="Sign in" />
+            <Text>mensagem de status...</Text>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({})
 
 export default LoginScreen
