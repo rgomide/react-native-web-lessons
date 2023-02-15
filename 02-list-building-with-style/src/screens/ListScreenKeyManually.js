e411dc8a6dee2c466cbb6f6d65221fc0e812fa69
rgomide <renato.s.gomide@gmail.com>
2023-02-15 19:49:34 -0300

    .

--- 02-list-building-with-style/src/screens/ListScreenKeyManually.js
+++ 02-list-building-with-style/src/screens/ListScreenKeyManually.js
@@ -3,31 +3,31 @@ import React from 'react'
 import { Text, StyleSheet, FlatList, View } from 'react-native'
 
 const ListScreenKeyManually = () => {
-    // CREATE AN ARRAY OF OBJECTS WITH key PROPERTY
-    const friends = [
-        { name: 'Friend #1', key: '1' },
-        { name: 'Friend #2', key: '2' },
-        { name: 'Friend #3', key: '3' },
-        { name: 'Friend #4', key: '4' },
-        { name: 'Friend #5', key: '5' },
-        { name: 'Friend #6', key: '6' },
-        { name: 'Friend #7', key: '7' },
-        { name: 'Friend #8', key: '8' },
-        { name: 'Friend #9', key: '9' }
-    ]
+  // CREATE AN ARRAY OF OBJECTS WITH key PROPERTY
+  const friends = [
+    { name: 'Friend #1', key: '1' },
+    { name: 'Friend #2', key: '2' },
+    { name: 'Friend #3', key: '3' },
+    { name: 'Friend #4', key: '4' },
+    { name: 'Friend #5', key: '5' },
+    { name: 'Friend #6', key: '6' },
+    { name: 'Friend #7', key: '7' },
+    { name: 'Friend #8', key: '8' },
+    { name: 'Friend #9', key: '9' }
+  ]
 
-    return (
-        <View>
-            <FlatList
-                data={friends}
-                renderItem={
-                    (element) => {
-                        // element === { item: { name: 'Friend #1', key: '1' }, index: 0 }
-                        return <Text>{element.item.name}</Text>
-                    }
-                } />
-        </View>
-    )
+  return (
+    <View>
+      <FlatList
+        data={friends}
+        renderItem={
+          (element) => {
+            // element === { item: { name: 'Friend #1', key: '1' }, index: 0 }
+            return <Text>{element.item.name}</Text>
+          }
+        } />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({})
