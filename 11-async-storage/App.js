117517db3c26e4bd4fd3074da241385a0bed8657
rgomide <renato.s.gomide@gmail.com>
2023-05-10 16:22:58 -0300

    .

--- 11-async-storage/App.js
+++ 11-async-storage/App.js
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   const [value, setValue] = useState('')
+
+  // useEffect to clear storage when load and close App
+  useEffect(() => {
+    clear()
+    return () => {
+      clear()
+    }
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Let's test AsyncStorage</Text>
@@ -22,6 +31,10 @@ export default function App() {
       <StatusBar style="auto" />
     </View>
   );
+}
+
+async function clear() {
+  await AsyncStorage.clear()
 }
 
 async function save(key, value) {
