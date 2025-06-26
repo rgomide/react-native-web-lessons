fdc6555df4fa9cbc4119e401c1a8a2ffadba7507
rgomide <renato.s.gomide@gmail.com>
2025-06-26 08:52:58 -0300

    .

--- 11-async-storage/App.js
+++ 11-async-storage/App.js
@@ -30,15 +30,32 @@ export default function App() {
               name: 'Denecley'
             }
 
-            save('someKey', JSON.stringify(user))
+            const arrayOfUsers = [
+              {
+                id: 1,
+                name: 'John'
+              },
+              {
+                id: 2,
+                name: 'Doe'
+              }
+            ]
+
+            save('user', JSON.stringify(user))
+            save('users', JSON.stringify(arrayOfUsers))
+            save('someKey', 'some value!')
           }} />
         <Button
           title="Load"
           onPress={async () => {
-            const valueFromStorage = await load('someKey')
-            const user = JSON.parse(valueFromStorage)
+            const userFromStorage = await load('user')
+            const usersFromStorage = await load('users')
+            const someKeyFromStorage = await load('someKey')
 
-            setValue(user.name)
+            const user = JSON.parse(userFromStorage)
+            const users = JSON.parse(usersFromStorage)
+
+            setValue(user.name + '\n' + users[0].name + '\n' + users[1].name + '\n' + someKeyFromStorage)
           }} />
         <Button
           title="Clear"
